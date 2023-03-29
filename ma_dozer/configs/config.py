6cbb4be91d0970bbdd3a6cdbf56af98878b205c0
aviadet <aviad.et@gmail.com>
2023-03-29 23:13:04 +0300

    upload project

---
+++ ma_dozer/configs/config.py
@@ -0,0 +1,17 @@
+from ma_dozer.configs.navigation_config import NavigationConfig
+from ma_dozer.configs.nodes_config import Topics, CameraNode, AlgoNode, DozerNode, DumperNode
+from ma_dozer.configs.pydantic_config import BaseConfig
+
+
+class Config(BaseConfig):
+
+    topics: Topics = Topics()
+
+    camera: CameraNode = CameraNode()
+    algo: AlgoNode = AlgoNode()
+    dozer: DozerNode = DozerNode()
+    dumper: DumperNode = DumperNode()
+
+    dozer_aruco_idx: int = 3
+    dumper_aruco_idx: int = 4
+    use_estimated_aruco_pose = False
